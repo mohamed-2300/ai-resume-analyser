420b1e8321848180c9a52b6eddb949dc74598bc1
mohamed-2300 <mohamedaitbourice@gmail.com>
2025-08-28 16:40:41 +0100

    implement the wipepage

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -336,21 +336,23 @@ export const usePuterStore = create<PuterStore>((set, get) => {
 
         return puter.ai.chat(
             [
+            {
+                role: "user",
+                content: [
                 {
-                    role: "user",
-                    content: [
-                        {
-                            type: "file",
-                            puter_path: path,
-                        },
-                        {
-                            type: "text",
-                            text: message,
-                        },
-                    ],
+                    type: "file",
+                    puter_path: path,
                 },
+                {
+                    type: "text",
+                    text: message,
+                },
+                ],
+            },
             ],
             { model: "claude-3-7-sonnet" }
+            
+            // https://puter.com/puterai/chat/models
         ) as Promise<AIResponse | undefined>;
     };
 
